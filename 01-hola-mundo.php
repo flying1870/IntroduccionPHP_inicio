78ceec0ddbe4bfebac0cd16aedbe24b8815c08c4
Julio <julitosanchez1870@gmail.com>
2022-07-23 11:26:12 -0400

    Primer Commit

---
+++ 01-hola-mundo.php
@@ -0,0 +1,13 @@
+
+ <?php
+
+echo print "Hola Mundo";
+
+
+ //print_r "Hola Mundo";
+
+var_dump("20");
+
+?>
+
+'<h1> Hola Mundo</h1>';
---
+++ 02-variables.php
@@ -0,0 +1,14 @@
+<?php include 'includes/header.php';
+
+
+echo "<h1> Hola Mundo</h1>";
+
+// print "Hola Mundo";
+
+
+var_dump("20");
+
+
+
+include 'includes/footer.php';
+?>
